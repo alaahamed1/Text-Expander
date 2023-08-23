74a46200ab61644ccd340a9810ff6308245a8885
alaahamed22 <alaa.hamed8000@gmail.com>
2023-08-24 00:58:32 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import Text from './Text' 
+// import Accordion from './Accordion';
+function App() {
+  return (
+    <div className="App">
+      <Text className='box'/>
+    </div>
+  );
+}
+export default App;
